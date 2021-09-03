38d5e66a9b9640fd7bb0da292ced1da7b7036f37
Jeffrey Miller <mill7707@umm9020-h2ytx12.morris.umn.edu>
2021-09-03 17:30:49 -0500

    Adds new commands to big_clean.sh file that zip the file and extracts it to the main directory.

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -14,10 +14,22 @@ user_dir="$(pwd)"
 
 temp_dir="$(mktemp -d)"
 
+#Stores base name of archive file without extention 
+
+base_name=$(basename "$clean_file" .tgz)
+
 #Extract components of file to be cleaned into temperary file
 
 tar -xzf "$clean_file" -C "$temp_dir"
 
 #deletes every file in the temperary directory with "DELTETE ME!" in it
 
-grep -lr "DELETE ME!" "temp_dir" | xargs rm -f
+grep -lr "DELETE ME!" "$temp_dir" | xargs rm -f 
+
+#Enters the temperary directory
+
+cd "$temp_dir" || exit
+
+#Extracts the cleaned directory to the main directory with its new name
+
+tar -czf "$user_dir"/cleaned_"$clean_file" "$base_name"
