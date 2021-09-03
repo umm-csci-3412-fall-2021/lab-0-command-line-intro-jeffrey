294a427bcf1713c5c1550f3c73479e8b7abd4697
Jeffrey Miller <mill7707@umm9020-h2ytx12.morris.umn.edu>
2021-09-03 17:00:00 -0500

    Adds code that creates a new directory, extracts all the files of the argument directory into it, and erases all files that contain "DETELE ME!".

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -2,4 +2,22 @@
 
 #This is the scripting file for the cleaning rountine
 
-echo "This file cleans!"
+#Uses first argument for file that must be cleaned
+
+clean_file=$1
+
+#Identifies main directory
+
+user_dir="$(pwd)"
+
+#Creates temperary directory
+
+temp_dir="$(mktemp -d)"
+
+#Extract components of file to be cleaned into temperary file
+
+tar -xzf "$clean_file" -C "$temp_dir"
+
+#deletes every file in the temperary directory with "DELTETE ME!" in it
+
+grep -lr "DELETE ME!" "temp_dir" | xargs rm -f
